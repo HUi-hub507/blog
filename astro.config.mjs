3b5f6a267fd612a5185ab45091f0c3b7086a96ec
HUi-hub507 <3306004173@qq.com>
2025-05-12 17:19:24 +0800

    修复

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://echocity.cn",
+	site: "https://blog.echocity.cn",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
